6ce75ea81ea9d52fd860f9b9881d316490354a62
Your Name <your@email.com>
2026-02-22 13:54:44 +0530

    Remove all localhost CrossOrigin annotations from controllers

--- src/main/java/com/snookerbooking/controller/BookingController.java
+++ src/main/java/com/snookerbooking/controller/BookingController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
-
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
